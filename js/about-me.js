b64fadcfc363c932805f2bc48de0412b4530ff30
MANPREET <mondair.manpreet@gmail.com>
2017-02-13 15:52:20 -0500

    First Assignment

---
+++ js/about-me.js
@@ -0,0 +1,23 @@
+var firstName = "Manpreet" ;
+var lastName = "Mondair" ;
+var fullName = firstName.concat(lastName); 
+var email = "mondair.manpreet@gmail.com";
+var city = "false";
+var numberofyerarsincollege = 5;
+var numberofpets = 0;
+var numberofhoursofsleep= 4.5 + 3.6;
+var genreofmusic= "HipHOP";
+var favoutiteshowandmovies= ["JungleBook", "Trolls", "Kabil"];
+var jobtitleinfiveyears;
+if (jobtitleinfiveyears=== undefined){
+    txt = "jobtitleinfiveyears is undefined";
+}else {
+    txt = "jobtitleinfiveyears is defined";
+}
+var lifewithoutjavascript = null;
+var year = new Date();
+year.setFullYear(2005);
+year = year.getFullYear();
+
+console.log(firstName, lastName, fullName, email, city, numberofyerarsincollege, numberofpets, numberofhoursofsleep, genreofmusic, favoutiteshowandmovies, jobtitleinfiveyears, lifewithoutjavascript, year);
+
